946b4788718aabee47de0fcec234f0ab9086e343
Kevin <kevindlib@gimail.com>
2026-03-17 10:41:00 +0400

    chore: comment out unused Link import

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
+// import Link from 'next/link'
 import {
   Award,
 } from 'lucide-react'
